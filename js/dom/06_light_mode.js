935d76ea68082d6bd5c6dc64ec8d4fdc63c3ee09
sherman <shermanperez0@gmail.com>
2022-10-25 17:34:18 -0900

    6th excercise - darkTheme with localStorage

---
+++ js/dom/06_light_mode.js
@@ -0,0 +1,61 @@
+/*
+
+nota: si quieres comparar con lo que tiene un atributo dentro mejor hazlo con el metodo element.getProperty() porque te decuelve lo que esta dentro de la etiqueta. Por ejemplo estaba comparando lo que me da $btnActivste.src que devolvia --> http://127.0.0.1:5500/img/blah/blha.png en vez de compararlo con lo que me devuele el element.getAttribute() que me devuelve lo que neecsito --> img/blah/blha.png
+
+*/
+
+const d = document;
+const localStorageKey = "theme-mode";
+
+const lightProps = {
+    green: "#F9F6EE",
+    black: "#477998",
+    img: "img/icon/luna.png",
+    theme: "light"
+}
+
+const darkProps = {
+    green: "#C4D6B0",
+    black: "#291F1E",
+    img: "img/icon/soleado.png",
+    theme: "dark"
+}
+
+export function setLightMode(btn){
+    const $btnActivate = d.querySelector(btn);
+    
+    const changeMode = ({green, black, img, theme})=>{
+        localStorage.setItem(localStorageKey, theme)
+        $btnActivate.src = img;    
+        d.documentElement.style.setProperty("--neutral-green", green)
+        d.documentElement.style.setProperty("--neutral-black", black)
+    }
+        
+    if(localStorage.getItem(localStorageKey) == lightProps.theme){
+        changeMode(lightProps)
+    }else{
+        changeMode(darkProps)
+    }
+    
+    d.addEventListener("click", (e)=>{
+        
+        if(!(e.target.matches(btn))) return;
+        
+        if(localStorage.getItem(localStorageKey) == darkProps.theme){
+            changeMode(lightProps)
+            return; // en vez del else
+        }
+        
+        changeMode(darkProps)
+
+    })
+
+}
+
+/*
+
+solucion del jonathan --> Curso JavaScript: 88. DOM: Ejercicios Prácticos 
+
+*/
+
+// solucion del jonathan - Curso JavaScript: 89. DOM: Ejercicios Prácticos | API localStorage 
--- js/index_dom.js
+++ js/index_dom.js
@@ -3,35 +3,42 @@ import {setClock, alarm} from "./dom/02_reloj_digital.js";
 import {getThePressedKey, setBolita, setShorcutAlert, setShorcutConfirm, setShorcutPrompt} from "./dom/03_eventos_del_teclado.js";
 import {setCountdown} from "./dom/04_countdown.js";
 import {scrollButton} from "./dom/05_scroll_button.js";
+import {setLightMode} from "./dom/06_light_mode.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e)=>{
+    
     // llamadas del ejecicio 1
     hamburgerMenu("#burguer-menu", "nav")
     hamburgerMenu(".img-close", "nav")
-
+    
     // llamadas del ejercicio 2
     setClock(".clock", ".startBtn", ".stopBtn")
     alarm(".playAlarmBtn", ".stopAlarmBtn")
-
+    
+    //llamadas del ejercicio 3
+    getThePressedKey()
+    setShorcutAlert("Has generado una alerta")
+    setShorcutConfirm("quieres realizar esta accion")
+    setShorcutPrompt("Escribe algun texto, te escucho")
+    
     //llamadas ejercicio 4
     setBolita(".bolita-container", ".bolita")
-
+    
+    //llamadas del ejercicio 5
+    let dateLimit = new Date(new Date().getFullYear()+1, new Date().getMonth(),new Date().getDate(), 10, 29)
+    setCountdown(".countdown-container", dateLimit)
+    // let dateLimit = new Date("Oct 23, 2022 22:37:25")
+    // console.log(dateLimit)
+    
     //llamadas ejercicio 6
     scrollButton(".top-button")
 
+    //llamadas ejercicio 7
+    setLightMode(".mode")
 })
 
 
-//llamadas del ejercicio 3
-getThePressedKey()
-setShorcutAlert("Has generado una alerta")
-setShorcutConfirm("quieres realizar esta accion")
-setShorcutPrompt("Escribe algun texto, te escucho")
+//llamadas ejercicio 7 - en la solucion del jonathan del darkMode se llama fuera porque no se puede tener un eventListener del mismo tipo de donde se agrega otro. o sea, en estre caso no se puede añadir un listener de DOMContentLoad dentro de un handler de un DOMContentLoad
 
-//llamadas del ejercicio 5
-let dateLimit = new Date(new Date().getFullYear() + 1, new Date().getMonth(),new Date().getDate(), 4, 37)
-setCountdown(".countdown-container", dateLimit)
-// let dateLimit = new Date("Oct 23, 2022 22:37:25")
-// console.log(dateLimit)

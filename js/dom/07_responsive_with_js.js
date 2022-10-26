8b686e3504df5c6831c02524aac9aba2c6b91494
sherman <shermanperez0@gmail.com>
2022-10-26 08:43:42 -0900

    7th excercise - responsible responsive design. mobile

---
+++ js/dom/07_responsive_with_js.js
@@ -0,0 +1,44 @@
+const w = window,
+    d = document;
+
+const elObjIterable = {
+    url: "https://www.youtube.com/watch?v=qU9mHegkTc4&ab_channel=ArcticMonkeys-Topic",
+
+    iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/UlOyFgR-xA8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+
+    select: ".irlYT",
+
+    name: "Ver video"
+}
+
+export function setMedia(minWidth, ...elObjIterable){ 
+    //recibirá varios objetos de tipo elObjIterable, que tienen la estructura de arriba.
+    const mql = w.matchMedia(`(min-width: ${minWidth}px)`);
+
+    const adaptToMedia = function(elObjIterable){
+        elObjIterable.forEach((el)=>{
+            const $el = document.querySelector(el.select);
+    
+            if(mql.matches){
+                $el.innerHTML = el.iframe
+            }else{
+                console.log(mql)
+                $el.innerHTML = `<a href="${el.url}" target="_blank">${el.name}</a>`
+            }
+        })
+    }
+
+    adaptToMedia(elObjIterable)
+
+    w.addEventListener("resize", (e)=>{
+        adaptToMedia(elObjIterable)
+    })
+
+
+}
+
+/*
+
+solucion del jonathan 
+
+*/
--- js/index_dom.js
+++ js/index_dom.js
@@ -4,6 +4,7 @@ import {getThePressedKey, setBolita, setShorcutAlert, setShorcutConfirm, setShor
 import {setCountdown} from "./dom/04_countdown.js";
 import {scrollButton} from "./dom/05_scroll_button.js";
 import {setLightMode} from "./dom/06_light_mode.js";
+import { setMedia } from "./dom/07_responsive_with_js.js";
 
 const d = document;
 
@@ -37,7 +38,33 @@ d.addEventListener("DOMContentLoaded", (e)=>{
 
     //llamadas ejercicio 7
     setLightMode(".mode")
+
+    //llamadas de l ejercicio 8
+    setMedia(700, ...objArray)
 })
+
+var objArray = [
+    {
+    url: "https://www.youtube.com/watch?v=qU9mHegkTc4&ab_channel=ArcticMonkeys-Topic",
+
+    iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/UlOyFgR-xA8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+
+    select: ".iframeYT",
+
+    name: "Ver video"
+    },
+    {
+        url: "https://goo.gl/maps/bjFodGaEQ4fGU9XUA",
+    
+        iframe: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3761.4460577079453!2d-70.69168113309999!3d19.479435858367655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eb1c5be08b867d9%3A0x235e79071ff0bdd!2sReparto%20Villalona%2C%20Santiago%20De%20Los%20Caballeros%2051000!5e0!3m2!1ses!2sdo!4v1666796240470!5m2!1ses!2sdo" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    
+        select: ".iframeGoogle",
+    
+        name: "Ver Mapa"
+    }
+]
+
+
 
 
 //llamadas ejercicio 7 - en la solucion del jonathan del darkMode se llama fuera porque no se puede tener un eventListener del mismo tipo de donde se agrega otro. o sea, en estre caso no se puede añadir un listener de DOMContentLoad dentro de un handler de un DOMContentLoad
